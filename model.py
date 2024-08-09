28c3f633d9eeb1d8fca7369ea053779ecdbb682f
Zoe <zoedroulias@gmail.com>
2024-08-09 16:40:25 -0400

    add checkLetterCounts() to generateLetters() (renamed the function for generating letters)

--- model.py
+++ model.py
@@ -37,12 +37,13 @@ count = {
         "a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h": 0, "i": 0, "j": 0, "k": 0, "l": 0, "m": 0, "n": 0, "o": 0, "p": 0, "q": 0, "r": 0, "s": 0, "t": 0, "u": 0, "v": 0, "w": 0, "x": 0, "y": 0, "z": 0
     }
 
-def generate_letters():
+def generateLetters():
     letters = [] # keep track of letters generated
     
     for i in range(25):
         letter = random.choice(alphabet)
         updateLetterCount(letter)
+        checkLetterCounts(letters)
 
 def updateLetterCount(letter):
     if letter == "A":
