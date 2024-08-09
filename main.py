87302e638910ed5452645c4d35c2725dc05a14d2
Zoe <zoedroulias@gmail.com>
2024-08-09 16:55:00 -0400

    update the letters in the shuffle route

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def home():
 def shuffle():
     letters = generateLetters()
     
-    return render_template("index.html", letter1="X", letter2="X", letter3="X", letter4="X", letter5="X", letter6="X", letter7="X", letter8="X", letter9="X", letter10="X", letter11="X", letter12="X", letter13="X", letter14="X", letter15="X", letter16="X", letter17="X", letter18="X", letter19="X", letter20="X", letter21="X", letter22="X", letter23="X", letter24="X", letter25="X")
+    return render_template("index.html", letter1=letters[0], letter2=letters[1], letter3=letters[2], letter4=letters[3], letter5=letters[4], letter6=letters[5], letter7=letters[6], letter8=letters[7], letter9=letters[8], letter10=letters[9], letter11=letters[10], letter12=letters[11], letter13=letters[12], letter14=letters[13], letter15=letters[14], letter16=letters[15], letter17=letters[16], letter18=letters[17], letter19=letters[18], letter20=letters[19], letter21=letters[20], letter22=letters[21], letter23=letters[22], letter24=letters[23], letter25=letters[24])
 
 app.run(debug=True)
