94c6984e0bcc3eac5fd5399e7fd6fed8c6760ff5
Zoe <zoedroulias@gmail.com>
2024-08-09 16:22:11 -0400

    move the dict for the letter counts and update the count so it's its own function

--- model.py
+++ model.py
@@ -31,17 +31,18 @@ Z_COUNT = 1
 
 # generate list of letters
 alphabet = list(ascii_uppercase())
+count = {
+        "a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h": 0, "i": 0, "j": 0, "k": 0, "l": 0, "m": 0, "n": 0, "o": 0, "p": 0, "q": 0, "r": 0, "s": 0, "t": 0, "u": 0, "v": 0, "w": 0, "x": 0, "y": 0, "z": 0
+    }
 
 def generate_letters():
     letters = [] # keep track of letters generated
     
-    count = {
-        "a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h": 0, "i": 0, "j": 0, "k": 0, "l": 0, "m": 0, "n": 0, "o": 0, "p": 0, "q": 0, "r": 0, "s": 0, "t": 0, "u": 0, "v": 0, "w": 0, "x": 0, "y": 0, "z": 0
-    }
-    
     for i in range(25):
         letter = random.choice(alphabet)
-        
+        updateLetterCount(letter)
+
+def updateLetterCount(letter):
         if letter == "A":
             count["a"] += 1
         if letter == "B":
