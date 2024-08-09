3db926b3a8386f2b9c9e846694e725332432965c
Zoe <zoedroulias@gmail.com>
2024-08-09 16:53:23 -0400

    remove temporary variables in home route; give the generated letters from generateLetters() a var name so it could be accessed

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html", letter1="X", letter2="X", letter3="X", letter4="X", letter5="X", letter6="X", letter7="X", letter8="X", letter9="X", letter10="X", letter11="X", letter12="X", letter13="X", letter14="X", letter15="X", letter16="X", letter17="X", letter18="X", letter19="X", letter20="X", letter21="X", letter22="X", letter23="X", letter24="X", letter25="X")
+    return render_template("index.html")
 
 @app.route("/shuffle")
 def shuffle():
-    generate_letters()
+    letters = generateLetters()
     
     return render_template("index.html", letter1="X", letter2="X", letter3="X", letter4="X", letter5="X", letter6="X", letter7="X", letter8="X", letter9="X", letter10="X", letter11="X", letter12="X", letter13="X", letter14="X", letter15="X", letter16="X", letter17="X", letter18="X", letter19="X", letter20="X", letter21="X", letter22="X", letter23="X", letter24="X", letter25="X")
 
