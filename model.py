dd3f4e9e363b7bf5ce29bd8db6c3a6f70e95e18f
Zoe <zoedroulias@gmail.com>
2024-08-09 16:04:15 -0400

    add occurrences for the letters in boggle

--- model.py
+++ model.py
@@ -1,2 +1,29 @@
+A_COUNT = 12
+B_COUNT = 1
+C_COUNT = 5
+D_COUNT = 6
+E_COUNT = 19
+F_COUNT = 4
+G_COUNT = 3
+H_COUNT = 5
+I_COUNT = 11
+J_COUNT = 1
+K_COUNT = 1
+L_COUNT = 5
+M_COUNT = 4
+N_COUNT = 11
+O_COUNT = 11
+P_COUNT = 4
+Q_COUNT = 1
+R_COUNT = 12
+S_COUNT = 9
+T_COUNT = 13
+U_COUNT = 4
+V_COUNT = 1
+W_COUNT = 2
+X_COUNT = 1
+Y_COUNT = 3
+Z_COUNT = 1
+
 def generate_letters():
     pass
