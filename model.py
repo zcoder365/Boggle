8348c92472a73c8e8129e0c374d49d5eeb9c0992
Zoe <zoedroulias@gmail.com>
2024-08-10 11:31:01 -0400

    add a method for replacing letters

--- model.py
+++ model.py
@@ -15,7 +15,6 @@ count = {
 
 def generateLetters():
     letters = [] # keep track of letters generated
-    letters_len = len(letters)
     
     for i in range(1, 26):
         letter = random.choice(alphabet)
@@ -30,7 +29,15 @@ def checkLetterCount(letters: list):
     for letter in letters:
         
         if count[letter.lower()] <= FINAL_COUNT[letter.lower()]:
-            print(f"{letter} is good.")
+            pass
         
         elif count[letter.lower()] > FINAL_COUNT[letter.lower()]:
             print(f"{letter} at index {letters.index(letter)} exceeds letter count.")
+            
+            
+def replaceLetter(letters_list: list, index: int):
+    new_letter = random.choice(alphabet)
+    letters_list[index] = new_letter
+    count[new_letter.lower()] += 1
+    
+    print(f"{letters_list[index]} at index {index} replaced by {new_letter}.")
