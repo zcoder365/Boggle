daf7a1865123441e5c3c5acdf10803b8307205e0
Zoe <zoedroulias@gmail.com>
2024-08-10 10:49:32 -0400

    update variables

--- main.py
+++ main.py
@@ -7,6 +7,32 @@ app = Flask(__name__)
 def home():
     letters = list(generateLetters())
     
-    return render_template("index.html", letter1=letters[0], letter2=letters[1], letter3=letters[2], letter4=letters[3], letter5=letters[4], letter6=letters[5], letter7=letters[6], letter8=letters[7], letter9=letters[8], letter10=letters[9], letter11=letters[10], letter12=letters[11], letter13=letters[12], letter14=letters[13], letter15=letters[14], letter16=letters[15], letter17=letters[16], letter18=letters[17], letter19=letters[18], letter20=letters[19], letter21=letters[20], letter22=letters[21], letter23=letters[22], letter24=letters[23], letter25=letters[24])
+    one = letters[0]
+    two = letters[1]
+    three = letters[2]
+    four = letters[3]
+    five = letters[4]
+    six = letters[5]
+    seven = letters[6]
+    eight = letters[7]
+    nine = letters[8]
+    ten = letters[9]
+    eleven = letters[10]
+    twelve = letters[11]
+    thirteen = letters[12]
+    fourteen = letters[13]
+    fifteen = letters[14]
+    sixteen = letters[15]
+    seventeen = letters[16]
+    eighteen = letters[17]
+    nineteen = letters[18]
+    twenty = letters[19]
+    twentyOne = letters[20]
+    twentyTwo = letters[21]
+    twentyThree = letters[22]
+    twentyFour = letters[23]
+    twentyFive = letters[24]
+    
+    return render_template("index.html", letter1=one, letter2=two, letter3=three, letter4=four, letter5=five, letter6=six, letter7=seven, letter8=eight, letter9=nine, letter10=ten, letter11=eleven, letter12=twelve, letter13=thirteen, letter14=fourteen, letter15=fifteen, letter16=sixteen, letter17=seventeen, letter18=eighteen, letter19=nineteen, letter20=twenty, letter21=twentyOne, letter22=twentyTwo, letter23=twentyThree, letter24=twentyFour, letter25=twentyFive)
 
 app.run(debug=True)
