8b3a2e3eb9d33dcd910d2cfeaeb27677e0255c8e
Zoe <zoedroulias@gmail.com>
2024-08-10 10:27:08 -0400

    update spacing of function and add some comments

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 function timer(){
-    var sec = 30;
-    var timer = setInterval(function(){
-        document.getElementById('safeTimerDisplay').innerHTML='00:'+sec;
-        sec--;
+    var sec = 180; // set seconds to 180 (180s = 3m)
+    
+    var timer = setInterval( function() {
+        // update the timer on the page
+        document.getElementById('timerDisplay').innerHTML=(sec%60)+':'+sec;
+       
+        sec--; // decrease the seconds by 1
+    
+        // if the timer goes below 0
         if (sec < 0) {
+            // clear the interval
             clearInterval(timer);
         }
+    
     }, 1000);
+
 }
