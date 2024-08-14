15b5c37cac05741a7cc060d0035157ba0c309606
Zoe <zoedroulias@gmail.com>
2024-08-14 16:36:13 -0400

    add letters to page

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def home():
     letters = getRandomLetters()
     shuffled_letters = shuffleDice(letters)
     
-    return render_template("index.html", letters=shuffled_letters)
+    return render_template("index.html", letter1=shuffled_letters[0], letter2=shuffled_letters[1], letter3=shuffled_letters[2], letter4=shuffled_letters[3], letter5=shuffled_letters[4], letter6=shuffled_letters[5], letter7=shuffled_letters[6], letter8=shuffled_letters[7], letter9=shuffled_letters[8], letter10=shuffled_letters[9], letter11=shuffled_letters[10], letter12=shuffled_letters[11], letter13=shuffled_letters[12], letter14=shuffled_letters[13], letter15=shuffled_letters[14], letter16=shuffled_letters[15], letter17=shuffled_letters[16], letter18=shuffled_letters[17], letter19=shuffled_letters[18],letter20=shuffled_letters[19], letter21=shuffled_letters[20], letter22=shuffled_letters[21], letter23=shuffled_letters[22], letter24=shuffled_letters[23], letter25=shuffled_letters[24])
 
 @app.route('/get_time')
 def get_time():
